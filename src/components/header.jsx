be5d631d5d2c9e9b92fe5423010cf381540cbe03
Haditya <hadityafajri.hf@gmail.com>
2021-04-30 14:35:55 +0700

    init

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo"></div>
+      <div className="header__main"></div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./sidebar.css";
+
+export const Sidebar = () => {
+  return <div className="sidebar">Sidebar</div>;
+};
